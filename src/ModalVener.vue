2ba55fe7aa86bc629eae61a138b1d51a9cc0a52d
dlwlckdWkd12 <jccc23@naver.com>
2023-09-14 21:53:00 +0900

    Vmodel

--- src/ModalVener.vue
+++ src/ModalVener.vue
@@ -4,9 +4,11 @@
         <img :src="data[productDetail].image" style='width: 60%' >
         <h4>{{data[productDetail].title}}</h4>
         <p>{{data[productDetail].content}}</p>
-        <p>{{data[productDetail].price}} 원</p>
+        <!-- <input @input="month=$event.target.value"> -->
+        <input v-model.number="month">
+        <p> {{month}}개월 선택함 {{data[productDetail].price * month}} 원</p>
         <button @click="cancle">닫기</button>
-        <!-- <button @click="modal = false">닫기</button> -->
+        
       </div>
     </div>
 </template>
@@ -14,6 +16,11 @@
 <script>
 export default {
     name:'modalInfo',
+    data(){
+      return{
+        month : 1,
+      }
+    },
     props : {
       data : Array,
       productDetail : Number,
