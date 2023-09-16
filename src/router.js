78f4614ae65b413b38a5c9dd608ad259ea440da6
dlwlckdWkd12 <jccc23@naver.com>
2023-09-16 22:59:49 +0900

    router

---
+++ src/router.js
@@ -0,0 +1,23 @@
+
+    //controller
+// const routes = [
+//     {
+//       path: '/detail/0',
+//       component: Detail,
+//     },
+//     {
+//       path: '/detail/1',
+//       component: Detail,
+//     },
+//     ~~ 계속
+//   ];
+
+const routes = [
+    {
+      path: '/detail/:id',
+      component: Detail,
+    },
+  ];
+
+
+  //{{ $route.params.파라미터명 }}
